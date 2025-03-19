543fe7e01efc4c0ec247d7ccdd82ee9fc3a6df3f
dean <thinkpad>
2025-03-19 04:24:43 -0400

    edit xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,12 +3,13 @@ import System.Exit
 import XMonad.Util.Run
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.SpawnOnce
+import XMonad.Util.NamedScratchpad
+import XMonad.Prompt.OrgMode (orgPrompt)
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
 myTerminal      = "xfce4-terminal"
-
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 
@@ -21,114 +22,79 @@ myFocusedBorderColor = "#44475A"
 
 myModMask       = mod4Mask
 
-myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
+myWorkspaces    = ["1","2","3","4","5","6"]
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
-    -- launch a terminal
     [ ((modm, xK_Return), spawn $ XMonad.terminal conf)
-    -- launch dmenu
-    , ((modm,               xK_d     ), spawn "dmenu_run")
-    -- launch rofi
-    , ((modm .|. shiftMask, xK_r     ), spawn "rofi -show drun")
-
-    -- close focused window
+    , ((modm,               xK_d     ), spawn "rofi -show drun")
+    , ((modm .|. shiftMask, xK_d     ), spawn "dmenu_run")
+    , ((modm .|. shiftMask, xK_o     ), orgPrompt def "TODO" "~/org/todos.org")
+    , ((modm .|. controlMask, xK_m     ), namedScratchpadAction myScratchPads "cmus")
+    , ((modm .|. controlMask, xK_Return), namedScratchpadAction myScratchPads "terminal")
+    , ((modm,               xK_b     ), spawn "librewolf")
+    , ((modm .|. shiftMask, xK_b     ), spawn "xfce4-terminal -e links")
     , ((modm .|. shiftMask, xK_c     ), kill)
-     -- Rotate through the available layout algorithms
     , ((modm,               xK_space ), sendMessage NextLayout)
-    --  Reset the layouts on the current workspace to default
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-    -- Resize viewed windows to the correct size
     , ((modm,               xK_n     ), refresh)
-    -- Move focus to the next window
     , ((modm,               xK_Tab   ), windows W.focusDown)
-    -- Move focus to the next window
     , ((modm,               xK_j     ), windows W.focusDown)
-    -- Move focus to the previous window
     , ((modm,               xK_k     ), windows W.focusUp  )
-    -- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster  )
-    -- Swap the focused window and the master window
     , ((modm,               xK_p), windows W.swapMaster)
-    -- Swap the focused window with the next window
     , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-    -- Swap the focused window with the previous window
     , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
-    -- Shrink the master area
     , ((modm,               xK_h     ), sendMessage Shrink)
-    -- Expand the master area
     , ((modm,               xK_l     ), sendMessage Expand)
-    -- Push window back into tiling
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-    -- Increment the number of windows in the master area
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-    -- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
     -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
     -- Restart xmonad
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
 
-    -- Run xmessage with a summary of the default keybindings (useful for beginners)
+      -- Run xmessage with a summary of the default keybindings (useful for beginners)
     , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
     ]
     ++
-    --
-    -- mod-[1..9], Switch to workspace N
-    -- mod-shift-[1..9], Move client to workspace N
-    --
+    -- mod-[1..6], Switch to workspace N
+    -- mod-shift-[1..6], Move client to workspace N
     [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_6]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
-    --
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 
-    -- mod-button1, Set the window to floating mode and move by dragging
     [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
                                        >> windows W.shiftMaster))
-
-    -- mod-button2, Raise the window to the top of the stack
     , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
-
-    -- mod-button3, Set the window to floating mode and resize by dragging
     , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
                                        >> windows W.shiftMaster))
-
-    -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
 
 myLayout = avoidStruts tiled ||| Mirror tiled ||| Full
   where
-     -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
-     -- The default number of windows in the master pane
      nmaster = 1
-     -- Default proportion of screen occupied by master pane
      ratio   = 1/2
-     -- Percent of screen to increment by when resizing panes
      delta   = 3/100
 
 myManageHook = composeAll
     [ className =? "MPlayer"        --> doFloat
-    , className =? "Gimp"           --> doFloat
+    , className =? "GIMP"           --> doFloat
     , resource  =? "desktop_window" --> doIgnore
-    , resource  =? "kdesktop"       --> doIgnore ]
-
+    , resource  =? "kdesktop"       --> doIgnore 
+    ] <+> namedScratchpadManageHook myScratchPads
+    
 -- EVENT HANDLING
 myEventHook = mempty
 -- STATUS BARS / LOGGING
@@ -138,6 +104,29 @@ myStartupHook = do
               spawnOnce "xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal &"
               spawnOnce "compton &"
               spawnOnce "nitrogen --restore &"
+
+-- SCRATCHPADS
+myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
+                , NS "cmus" spawnCmus findCmus manageCmus
+                ]
+
+    where 
+    spawnTerm = myTerminal ++ " -T scratchpad"
+    findTerm = title =? "scratchpad"
+    manageTerm = customFloating $ W.RationalRect l t w h
+                 where
+                 h = 0.9
+                 w = 0.9
+                 t = 0.95 -h
+                 l = 0.95 -w
+    spawnCmus = myTerminal ++ " -T cmus -e 'cmus'"
+    findCmus = title =? "cmus"
+    manageCmus = customFloating $ W.RationalRect l t w h
+                 where
+                 h = 0.9
+                 w = 0.9
+                 t = 0.95 -h
+                 l = 0.95 -w
 
 main = do
   xmproc <- spawnPipe "xmobar"
@@ -203,10 +192,10 @@ help = unlines ["The default modifier key is 'alt'. Default keybindings:",
     "-- quit, or restart",
     "mod-Shift-q  Quit xmonad",
     "mod-q        Restart xmonad",
-    "mod-[1..9]   Switch to workSpace N",
+    "mod-[1..6]   Switch to workSpace N",
     "",
     "-- Workspaces & screens",
-    "mod-Shift-[1..9]   Move client to workspace N",
+    "mod-Shift-[1..6]   Move client to workspace N",
     "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
     "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
     "",
@@ -214,3 +203,4 @@ help = unlines ["The default modifier key is 'alt'. Default keybindings:",
     "mod-button1  Set the window to floating mode and move by dragging",
     "mod-button2  Raise the window to the top of the stack",
     "mod-button3  Set the window to floating mode and resize by dragging"]
+
