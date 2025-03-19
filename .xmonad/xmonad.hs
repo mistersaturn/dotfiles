bbec66aa9a0c099e57a34ebf41989be23d518113
dean <thinkpad>
2025-03-19 17:08:25 -0400

    edit xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,24 +5,23 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.SpawnOnce
 import XMonad.Util.NamedScratchpad
 import XMonad.Prompt.OrgMode (orgPrompt)
-
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
-myTerminal      = "xfce4-terminal"
+myTerminal = "xfce4-terminal"
+
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
-
 myClickJustFocuses :: Bool
 myClickJustFocuses = False
 
-myBorderWidth   = 2
-myNormalBorderColor  = "#282A36"
+myBorderWidth = 2
+myNormalBorderColor = "#282A36"
 myFocusedBorderColor = "#44475A"
 
-myModMask       = mod4Mask
+myModMask = mod4Mask
 
-myWorkspaces    = ["1","2","3","4","5","6"]
+myWorkspaces = ["1","2","3","4","5","6"]
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
@@ -30,7 +29,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm,               xK_d     ), spawn "rofi -show drun")
     , ((modm .|. shiftMask, xK_d     ), spawn "dmenu_run")
     , ((modm .|. shiftMask, xK_o     ), orgPrompt def "TODO" "~/org/todos.org")
-    , ((modm .|. controlMask, xK_m     ), namedScratchpadAction myScratchPads "cmus")
+    , ((modm .|. controlMask, xK_m   ), namedScratchpadAction myScratchPads "cmus")
     , ((modm .|. controlMask, xK_Return), namedScratchpadAction myScratchPads "terminal")
     , ((modm,               xK_b     ), spawn "librewolf")
     , ((modm .|. shiftMask, xK_b     ), spawn "xfce4-terminal -e links")
@@ -50,24 +49,15 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-
-    -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-    -- Restart xmonad
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
 
-      -- Run xmessage with a summary of the default keybindings (useful for beginners)
-    , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
     ]
     ++
-    -- mod-[1..6], Switch to workspace N
-    -- mod-shift-[1..6], Move client to workspace N
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_6]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
@@ -83,34 +73,30 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 
 myLayout = avoidStruts tiled ||| Mirror tiled ||| Full
   where
-     tiled   = Tall nmaster delta ratio
+     tiled = Tall nmaster delta ratio
      nmaster = 1
-     ratio   = 1/2
-     delta   = 3/100
+     ratio = 1/2
+     delta = 3/100
 
 myManageHook = composeAll
     [ className =? "MPlayer"        --> doFloat
     , className =? "GIMP"           --> doFloat
     , resource  =? "desktop_window" --> doIgnore
-    , resource  =? "kdesktop"       --> doIgnore 
+    , resource  =? "kdesktop"       --> doIgnore
     ] <+> namedScratchpadManageHook myScratchPads
-    
--- EVENT HANDLING
+
 myEventHook = mempty
--- STATUS BARS / LOGGING
 myLogHook = return ()
--- STARTUP HOOK
 myStartupHook = do
               spawnOnce "xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal &"
               spawnOnce "compton &"
               spawnOnce "nitrogen --restore &"
 
--- SCRATCHPADS
 myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
                 , NS "cmus" spawnCmus findCmus manageCmus
                 ]
 
-    where 
+    where
     spawnTerm = myTerminal ++ " -T scratchpad"
     findTerm = title =? "scratchpad"
     manageTerm = customFloating $ W.RationalRect l t w h
@@ -131,7 +117,6 @@ myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ docks defaults
---xmonad defaults
 
 defaults = def {
       -- simple stuff
@@ -153,54 +138,3 @@ defaults = def {
         logHook            = myLogHook,
         startupHook        = myStartupHook
     }
-
-help :: String
-help = unlines ["The default modifier key is 'alt'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Shift-Enter  Launch xterminal",
-    "mod-p            Launch dmenu",
-    "mod-Shift-p      Launch gmrun",
-    "mod-Shift-c      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "",
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "",
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "mod-[1..6]   Switch to workSpace N",
-    "",
-    "-- Workspaces & screens",
-    "mod-Shift-[1..6]   Move client to workspace N",
-    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
-    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
-    "",
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging"]
-
